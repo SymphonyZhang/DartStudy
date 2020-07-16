fc418f0b3cb54b7c8f8730e29f7df42d5df92387
yx.Zhang <yx.zhang@byteflyer.com>
2020-07-16 15:53:06 +0800

    类初始化列表2

--- 04/03_类的初始化列表.dart
+++ 04/03_类的初始化列表.dart
@@ -1,13 +1,19 @@
 main(List<String> args) {
-  var p = Person('why');
+  var p = Person('why', age: 18);
+  print(p.age);
 }
+
+const temp = 20;
 
 class Person {
   final String name;
   final int age;
 
-  Person(this.name, {int age}) : this.age = age ?? 10 {}
+  //如果创建对象时，有传入一个age，那么就是用传入的age，如果没有传入age，那么使用一个默认值
+  Person(this.name, {int age}) : this.age = temp > 20 ? 30 : 50 {}
 
+  //这种写法只能做确定的复制,不能赋值一个表达式
+  //Person(this.name, {this.age = 10});
   //保留
   //Person(this.name, {int age = 10});
 }
